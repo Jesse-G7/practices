79e46e3b6aa2e87b5e814390379966f38ed7a0ff
Jesse-G7 <59207187+Jesse-G7@users.noreply.github.com>
2021-07-06 15:10:34 +0800

    pull test commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -71,5 +71,6 @@ public class Main {
         System.out.println("hot-fix branch");
         System.out.println("master commit");
         System.out.println("hot-fix commit2");
+        System.out.println("pull test");
     }
 }
